03271f88f14c6e1260545111109df86d26922997
Daniel Nguyen <dnlnguy@gmail.com>
2024-10-03 23:10:52 -0500

    feat(8080_emulator): add opcodes

--- 8080_emulator/main.c
+++ 8080_emulator/main.c
@@ -36,31 +36,54 @@ returns the number of bytes of the op
 */    
 
 int Disassemble8080Op(unsigned char *codebuffer, int pc)    
-{    
-    unsigned char *code = &codebuffer[pc];    
-    int opbytes = 1;    
-    printf ("%04x ", pc);    
-    switch (*code)    
-    {    
-        case 0x00: printf("NOP"); break;    
-        case 0x01: printf("LXI    B,#$%02x%02x", code[2], code[1]); opbytes=3; break;    
-        case 0x02: printf("STAX   B"); break;    
-        case 0x03: printf("INX    B"); break;    
-        case 0x04: printf("INR    B"); break;    
-        case 0x05: printf("DCR    B"); break;    
-        case 0x06: printf("MVI    B,#$%02x", code[1]); opbytes=2; break;    
-        case 0x07: printf("RLC"); break;    
-        case 0x08: printf("NOP"); break;    
-        /* ........ */    
-        case 0x3e: printf("MVI    A,#0x%02x", code[1]); opbytes = 2; break;    
-        /* ........ */    
-        case 0xc3: printf("JMP    $%02x%02x",code[2],code[1]); opbytes = 3; break;    
-        /* ........ */    
+{
+    unsigned char *code = &codebuffer[pc];
+    int opbytes = 1;
+    printf ("%04x ", pc);
+    switch (*code)
+    {
+        case 0x00: printf("NOP"); break;
+        case 0x01: printf("LXI    B,#$%02x%02x", code[2], code[1]); opbytes=3; break;
+        case 0x02: printf("STAX   B"); break;
+        case 0x03: printf("INX    B"); break;
+        case 0x04: printf("INR    B"); break;
+        case 0x05: printf("DCR    B"); break;
+        case 0x06: printf("MVI    B,#$%02x", code[1]); opbytes=2; break;
+        case 0x07: printf("RLC"); break;
+        case 0x08: printf("NOP"); break;
+        case 0x09: printf("DAD B"); break;
+        case 0x0A: printf("LDAX B"); break;
+        case 0x0B: printf("DCX B"); break;
+        case 0x0C: printf("INR C"); break;
+        case 0x0D: printf("DCR C"); break;
+        case 0x0E: printf("MVI C,#$%02x", code[1]); opbytes=2; break;
+        case 0x0F: printf("RRC"); break;
+        case 0x10: printf("NOP"); break;
+        case 0x11: printf("LXI    D,#$%02x%02x", code[2], code[1]); opbytes=3; break;
+        case 0x12: printf("STAX D"); break;
+        case 0x13: printf("INX D"); break;
+        case 0x14: printf("INR D"); break;
+        case 0x15: printf("DCR D"); break;
+        case 0x16: printf("MVI D,#$%02x", code[1]); opbytes=2; break;
+        case 0x17: printf("RAL"); break;
+        case 0x18: printf("NOP"); break;
+        case 0x19: printf("DAD D"); break;
+        case 0x1A: printf("LDAX D"); break;
+        /* ........ */
+        case 0x21: printf("LXI    H,#$%02x%02x", code[2], code[1]); opbytes=3; break;
+        /* ........ */
+        case 0x31: printf("LXI    SP,$%02x%02x",code[2],code[1]); opbytes = 3; break;
+        /* ........ */
+        case 0x3e: printf("MVI    A,#0x%02x", code[1]); opbytes = 2; break;
+        /* ........ */
+        case 0xc3: printf("JMP    $%02x%02x",code[2],code[1]); opbytes = 3; break;
+        /* ........ */
+        case 0xCD: printf("CALL   $%02x%02x",code[2],code[1]); opbytes = 3; break;
     }
 
     printf("\n");
 
-    return opbytes;    
+    return opbytes;
 }    
 
 void UnimplementedInstruction(State8080* state)    
@@ -73,8 +96,8 @@ void UnimplementedInstruction(State8080* state)
  void Emulate8080Op(State8080* state)    
 {    
     unsigned char *opcode = &state->memory[state->pc]; 
-    printf("PC: %u\n", state->pc);
-    printf("opcode: %x\n", *opcode); 
+    // printf("PC: %x\n", state->pc);
+    // printf("opcode: %x\n", *opcode); 
     Disassemble8080Op(state->memory, state->pc);
     switch(*opcode)    
     {    
@@ -84,11 +107,43 @@ void UnimplementedInstruction(State8080* state)
             state->b = opcode[2];    
             state->pc += 2;                  //Advance 2 more bytes    
             break;    
-        /*....*/    
+        /*....*/
+        case 0x06:
+            state->b = opcode[1];
+            state->pc += 1;
+            break;
+        /*....*/
+        case 0x11:
+            state->e = opcode[1];    
+            state->d = opcode[2];    
+            state->pc += 2;                  //Advance 2 more bytes    
+            break;
+        case 0x21:
+            state->l = opcode[1];    
+            state->h = opcode[2];    
+            state->pc += 2;                  //Advance 2 more bytes    
+            break;
+        case 0x31:
+            state->sp = ((opcode[2] << 8) | opcode[1]);
+            state->pc += 2;
+        /*....*/
         case 0x41: state->b = state->c; break;    //MOV B,C    
         case 0x42: state->b = state->d; break;    //MOV B,D    
         case 0x43: state->b = state->e; break;    //MOV B,E    
+        case 0xC3:
+            state->pc = ((opcode[2] << 8) | opcode[1]) - 1; break;
+        case 0xCD:
+            // (SP-1)<-PC.hi
+            // (SP-2)<-PC.lo
+            state->memory[(state->sp)-1] = state->pc >> 8 & 0xFF;
+            state->memory[(state->sp)-2] = state->pc & 0xFF;
+
+            state->sp -= 2;
+            state->pc = ((opcode[2] << 8) | opcode[1]) - 1;
+            break;
         default: 
+            printf("PC: %x\n", state->pc);
+            printf("OPCODE: %x\n", *opcode); 
             UnimplementedInstruction(state);      //quits program for unimplemented opcode for now
             break;
     }    
@@ -113,13 +168,9 @@ int main ()
     fread(state->memory, fsize, 1, f);    
     fclose(f);    
 
-    int pc = 0; 
-    
-
     while (1)    
     {    
         Emulate8080Op(state);
-        //pc += Disassemble8080Op(buffer, pc);    
     }    
 
 
